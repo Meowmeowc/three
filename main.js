fc50a2414a0fac6931fef3f2d043cf32a95b2c6c
Meowmeowc <lalachka26@mail.ru>
2023-01-21 15:55:18 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+let anime = 'Anime'
+
+// document.getElementById('one').innerHTML = '<h1>Hello</h1>';
+// document.getElementById('two').innerHTML = 666;
+// document.getElementById('one').innerHTML = 'anime'
+
+// document.querySelector('#one').innerHTML = 666;
+// document.querySelector('p').innerHTML = 'акуна матата'
+
+// let inPut = document.querySelector('.text')
+// let btn = document.querySelector('.check-in')
+// let div = document.querySelector('.out')
+// btn.onclick = function () {
+// 	console.log('После клика')
+// 	console.log(inPut.value)
+// 	let e = inPut.value
+// 	div.innerHTML = e
+// }
+
+
+// let myHappy = prompt('Когда у тебя др?')
+// console.log(myHappy)
+
+// if (myHappy < 22 && myHappy != '' && myHappy != null) {
+// 	alert('Ты очень маленький')
+// }
+// else if (myHappy => 22 && myHappy != '') {
+// 	alert('Ты в пожилом бро')
+// }
+// else {
+// 	alert('Ты неизвестного возраста')
+// }
+
+let test = prompt('число больше 100 ?')
+
+if (test > 100) {
+	document.write(test)
+}
+else
+	console.log(test)
